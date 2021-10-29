8bf02bef0006f861ad2b9e5bd7c56c99755c623a
NearMaick <maick_a_s@msn.com>
2021-10-29 15:51:57 -0300

    style(web): Home #1

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,8 +3,8 @@ import { ColorModeSwitcher } from '../ColorModeSwitcher'
 
 export function Header() {
   return (
-    <Flex w="100%" p="4" justifyContent="space-between">
-      <Heading>NearMaick.dev.br</Heading>
+    <Flex as="header" w="100%" p="4" justifyContent="space-between">
+      <Heading as="h1">NearMaick.dev.br</Heading>
       <ColorModeSwitcher />
     </Flex>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
 import type { NextPage } from 'next'
+import { Flex, Text, Image, Heading } from '@chakra-ui/react'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <p>Maick Souza</p>
-    </div>
+    <Flex as="main" height="90vh" justifyContent="center" alignItems="center">
+      <Image
+        src="https://i.ibb.co/KNqMjWF/tim-mossholder-tq8-Cuap8-w-Y-unsplash.jpg"
+        alt="teste"
+        w="100"
+        h="200"
+      />
+      <Heading as="h2">Falando sobre testes</Heading>
+      <Text>claro que essa ferramenta não é perfeita e de repente você precisa fazer algumas correções mas ganhei um baita tempo Principalmente quando você for querer fazer um blog tiver codando enquanto tá falando</Text>
+    </Flex>
   )
 }
 
