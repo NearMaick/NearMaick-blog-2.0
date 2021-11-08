583484ad97d140f07a028c9246d8265ee17359dc
NearMaick <maick_a_s@msn.com>
2021-11-08 14:44:17 -0300

    feat(web): landingPage #2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,75 +1,54 @@
-import type { GetStaticProps } from 'next'
-import Prismic from '@prismicio/client'
+import { Box, Flex, Image, Heading, Text, useColorMode } from '@chakra-ui/react'
 
-import { SimpleGrid } from '@chakra-ui/react'
-import { PostCard } from '../components/PostCard'
-import { LastPostCard } from '../components/LastPostCard'
-import { getPrismicClient } from '../services/prismic'
-
-type PostsProps = {
-  posts: {
-    uid: string | undefined
-    banner_url: string
-    banner_alt: string
-    title: string
-    subtitle: string
-  }[]
-}
-
-function Home({ posts }: PostsProps) {
-  const nextPosts = [...posts]
-
-  const lastPost = nextPosts.shift()
+export default function Home() {
+  const { colorMode } = useColorMode()
 
   return (
-    <>
-      <LastPostCard
-        uid={lastPost?.uid}
-        banner_url={lastPost?.banner_url}
-        banner_alt={posts[0].banner_alt}
-        title={lastPost?.title}
-        subtitle={lastPost?.subtitle}
-      />
-
-      <SimpleGrid minChildWidth="360px" spacingX="1" spacingY="14">
-        {nextPosts.map(post => (
-          <PostCard
-            key={post.uid}
-            uid={post.uid}
-            banner_url={post.banner_url}
-            banner_alt={post.banner_alt}
-            title={post.title}
-            subtitle={post.subtitle}
-          />
-        ))}
-      </SimpleGrid>
-    </>
+    <Box>
+      <Flex
+        direction={{ base: 'column-reverse', md: 'column-reverse', lg: 'row' }}
+        marginX="auto"
+      >
+        <Flex
+          flexDirection="column"
+          justifyContent="center"
+          w="container.sm"
+          h="lg"
+          marginY={{ base: 0, md: 0, lg: 'auto' }}
+          marginRight={{ base: 'auto', md: 'auto', lg: -100 }}
+          marginLeft={{ base: 'auto', md: 'auto', lg: '4' }}
+          marginTop={{ base: -100, md: -100, lg: 'auto' }}
+          paddingX="4"
+          zIndex="9"
+          background={colorMode === 'light' ? 'white' : 'gray.700'}
+          borderRadius="base"
+        >
+          <Heading fontSize="4xl" marginBottom="8">
+            Olá, sou o NearMaick
+          </Heading>
+          <Text>
+            Mussum Ipsum, cacilds vidis litro abertis. Praesent vel viverra
+            nisi. Mauris aliquet nunc non turpis scelerisque, eget. Praesent
+            malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate
+            dapibus. Interessantiss quisso pudia ce receita de bolis, mais bolis
+            eu num gostis. Casamentiss faiz malandris se pirulitá. Si num tem
+            leite então bota uma pinga aí cumpadi! Copo furadis é disculpa de
+            bebadis, arcu quam euismod magna. Quem manda na minha terra sou
+            euzis! Manduma pindureta quium dia nois paga. Suco de cevadiss, é um
+            leite divinis, qui tem lupuliz, matis, aguis e fermentis. Viva
+            Forevis aptent taciti sociosqu ad litora torquent. Per aumento de
+            cachacis, eu reclamis. Paisis, filhis, espiritis santis.
+          </Text>
+        </Flex>
+        <Image
+          src="https://github.com/nearmaick.png"
+          alt="NearMaick"
+          boxSize={700}
+          objectFit="contain"
+          borderRadius="base"
+          margin="auto"
+        />
+      </Flex>
+    </Box>
   )
-}
-
-export default Home
-
-export const getStaticProps: GetStaticProps = async () => {
-  const prismic = getPrismicClient()
-
-  const response = await prismic.query([
-    Prismic.predicates.at('document.type', 'posts'),
-  ])
-
-  const posts = response.results.map(post => {
-    return {
-      uid: post.uid,
-      banner_url: post.data.banner.url,
-      banner_alt: post.data.banner.alt,
-      title: post.data.title[0].text,
-      subtitle: post.data.subtitle[0].text,
-    }
-  })
-
-  return {
-    props: {
-      posts,
-    },
-    revalidate: 60 * 60 * 12, //12 hours
-  }
 }
