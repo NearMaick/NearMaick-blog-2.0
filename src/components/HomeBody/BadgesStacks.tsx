6f16f9cbe2b6ac50adae0c2b2ebc61a67fb5b82b
NearMaick <maick_a_s@msn.com>
2021-11-09 16:52:43 -0300

    style(web): HomeBody component #2

--- src/components/HomeBody/BadgesStacks.tsx
+++ src/components/HomeBody/BadgesStacks.tsx
@@ -1,6 +1,7 @@
-import { DiDart, DiErlang, DiNodejs, DiReact } from 'react-icons/di'
+import { DiNodejs, DiReact } from 'react-icons/di'
 
 import { Badge, Box, Flex, Image, Heading, Text } from '@chakra-ui/react'
+import { stacksBadges } from '../../utils/badgesStacks'
 
 export function BadgeStacks() {
   return (
@@ -9,29 +10,36 @@ export function BadgeStacks() {
         <Heading textAlign="center">
           Tecnologias que utilizo no momento...
         </Heading>
-        <Flex justifyContent="space-around">
-          <Badge>
+        <Flex justifyContent="center">
+          <Badge
+            display="flex"
+            flexDirection="column"
+            mx="8"
+            alignItems="center"
+            justifyContent="center"
+          >
             <DiNodejs size={60} />
             <Text>NodeJS</Text>
           </Badge>
-          <Badge>
+          <Badge
+            display="flex"
+            flexDirection="column"
+            mx="8"
+            alignItems="center"
+            justifyContent="center"
+          >
             <DiReact size={60} />
             <Text>ReactJS</Text>
           </Badge>
-        </Flex>
-        <Heading textAlign="center">O que estou aprendendo?</Heading>
-        <Flex justifyContent="space-around">
-          <Badge>
+          <Badge
+            display="flex"
+            flexDirection="column"
+            mx="8"
+            alignItems="center"
+            justifyContent="center"
+          >
             <DiReact size={60} />
             <Text>React Native</Text>
-          </Badge>
-          <Badge>
-            <DiErlang size={60} />
-            <Text>Elixir</Text>
-          </Badge>
-          <Badge>
-            <DiDart size={60} />
-            <Text>Flutter</Text>
           </Badge>
         </Flex>
       </Box>
@@ -40,14 +48,9 @@ export function BadgeStacks() {
           Outras Tecnologias que estou utilizando ao longo da minha jornada...
         </Heading>
         <Flex justifyContent="space-around">
-          <Image
-            src="https://img.shields.io/badge/chakra-%234ED1C5.svg?style=for-the-badge&logo=chakraui&logoColor=white"
-            alt="Chakra"
-          />
-          <Image
-            src="https://img.shields.io/badge/Next-black?style=for-the-badge&logo=next.js&logoColor=white"
-            alt="Next JS"
-          />
+          {stacksBadges.map(stack => (
+            <Image key={stack.alt} src={stack.src} alt={stack.alt} />
+          ))}
         </Flex>
       </Box>
     </>
---
+++ src/utils/badgesStacks.ts
@@ -0,0 +1,46 @@
+export const stacksBadges = [
+  {
+    src: 'https://img.shields.io/badge/Next-black?style=for-the-badge&logo=next.js&logoColor=white',
+    alt: 'Next JS',
+  },
+  {
+    src: 'https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white',
+    alt: 'HTML5',
+  },
+  {
+    src: 'https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white',
+    alt: 'Typescript',
+  },
+  {
+    src: 'https://img.shields.io/badge/chakra-%234ED1C5.svg?style=for-the-badge&logo=chakraui&logoColor=white',
+    alt: 'Chakra',
+  },
+  {
+    src: 'https://img.shields.io/badge/expo-1C1E24?style=for-the-badge&logo=expo&logoColor=#D04A37',
+    alt: 'expo',
+  },
+  {
+    src: 'https://img.shields.io/badge/Insomnia-black?style=for-the-badge&logo=insomnia&logoColor=5849BE',
+    alt: 'Insomnia',
+  },
+  {
+    src: 'https://img.shields.io/badge/JWT-black?style=for-the-badge&logo=JSON%20web%20tokens',
+    alt: 'JWT',
+  },
+  {
+    src: 'https://img.shields.io/badge/SASS-hotpink.svg?style=for-the-badge&logo=SASS&logoColor=white',
+    alt: 'SASS',
+  },
+  {
+    src: 'https://img.shields.io/badge/Socket.io-black?style=for-the-badge&logo=socket.io&badgeColor=010101',
+    alt: 'SocketIo',
+  },
+  {
+    src: 'https://img.shields.io/badge/styled--components-DB7093?style=for-the-badge&logo=styled-components&logoColor=white',
+    alt: 'Styled Components',
+  },
+  {
+    src: 'https://img.shields.io/badge/Visual%20Studio%20Code-0078d7.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white',
+    alt: 'VS Code',
+  },
+]
